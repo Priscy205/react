6e0d434df02e6abd582f5a32d08539f514de0a41
Priscy205 <prisslopez5@gmail.com>
2022-08-23 17:28:27 -0500

    Empezando a usar React sin Webpack y sin JSX y usando ReactDOM

--- avatars.js
+++ avatars.js
@@ -1,17 +1,26 @@
 const $app = document.getElementById("app")
-const Avatar = params => {
-    const src = `https://randomuser.me/api/portraits/women/${params.id}.jpg`;
-    return `<picture>
-        <img src= "${src}"/>
-        <strong>${params.name}</strong>
-    </picture>
-    `;
-};
-$app.innerHTML += Avatar({ id: 21, name: "Andrea"});
-$app.innerHTML += Avatar({ id: 48, name: "Daniela"});
+const h = React.createElement       //Crea un elemento
 
-$app.querySelectorAll('img').forEach(img => {
-    img.addEventListener('click', () => {
-        img.classList.toggle('disabled')
-    })
-})
+const Avatar = params => {
+    const src = `https://randomuser.me/api/portraits/women/${params.id}.jpg`
+    return h("img", {src})     //diciendole que es lo que queremos renderizar el elemento imagen y le vamos a pasar el atributo src
+};
+
+ReactDOM.render(h(Avatar, { id: 21}), $app);
+
+//const Avatar = params => {
+    //const src = `https://randomuser.me/api/portraits/women/${params.id}.jpg`;
+    //return `<picture>
+      //  <img src= "${src}"/>
+        //<strong>${params.name}</strong>
+    //</picture>
+    //`;
+//};
+//$app.innerHTML += Avatar({ id: 21, name: "Andrea"});
+//$app.innerHTML += Avatar({ id: 48, name: "Daniela"});
+
+//$app.querySelectorAll('img').forEach(img => {
+  //  img.addEventListener('click', () => {
+    //    img.classList.toggle('disabled')
+   // })
+//})
